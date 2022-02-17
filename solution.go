8c3df22fd36813b180bff524713f4f8ce491a963
Andrei Shalkou <andrei.shalkou@camelotls.com>
2022-02-17 17:38:31 +0300

    Added Square calculation functions

--- solution.go
+++ solution.go
@@ -7,6 +7,39 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+import "math"
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type intCustomType int8
+
+const (
+	SidesTriangle = 3
+	SidesSquare   = 4
+	SidesCircle   = 0
+)
+
+func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
+	switch sidesNum {
+	case SidesCircle:
+		return calcCircleSquare(sideLen)
+	case SidesTriangle:
+		return calcTriangleSquare(sideLen)
+	case SidesSquare:
+		return calcSquareSquare(sideLen)
+	default:
+		return 0
+	}
+}
+
+func calcCircleSquare(radius float64) float64 {
+	return math.Pi * radius * radius
+}
+
+func calcTriangleSquare(sideLen float64) float64 {
+	p := 3 * sideLen / 2
+	a := p - sideLen
+	return math.Sqrt(p * a * a * a)
+}
+
+func calcSquareSquare(sideLen float64) float64 {
+	return sideLen * sideLen
 }
